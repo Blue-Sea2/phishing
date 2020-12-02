ef9b82fa80effd513e74531a6c2818bb322a5520
Blue-Sea2 <65962153+Blue-Sea2@users.noreply.github.com>
2020-12-02 08:33:34 +0100

    Update save.js

--- save.js
+++ save.js
@@ -2,5 +2,5 @@ function submit(){
   var input = document.getElementById("email");
   var address = input.text;
   var database = firebase.database();
-  database.set({email: address});
+  database.set({email + Math.random() * 1000: address});
 }
