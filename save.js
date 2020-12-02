072debc08725ba957bd78af26c03aba53c4ad7b0
Blue-Sea2 <65962153+Blue-Sea2@users.noreply.github.com>
2020-12-02 08:34:06 +0100

    Delete save.js

--- save.js
+++
@@ -1,6 +0,0 @@
-function submit(){
-  var input = document.getElementById("email");
-  var address = input.text;
-  var database = firebase.database();
-  database.set({email + Math.random() * 1000: address});
-}
